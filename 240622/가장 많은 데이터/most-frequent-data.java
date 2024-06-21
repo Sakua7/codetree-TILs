dd332c02f387c9f1ee7d6a8141373b9467e355a5
Sakua7 <moonlighthm@icloud.com>
2024-06-22 00:29:18 +0900

    Title: 가장 많은 데이터, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240622/가장 많은 데이터/most-frequent-data.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = scanInt(br.readLine());
+        int max = 0;
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String color = scanString(br.readLine());
+            if (hm.get(color) == null) {
+                hm.put(color, 1);
+            } else {
+                int v = hm.get(color) + 1;
+                if (v > max) {
+                    max = v;
+                }
+                hm.put(color, v);
+            }
+        }
+
+        print(max);
+
+        bw.flush();
+        br.close();
+        bw.close();;
+    }
+
+    private static int scanInt(String token) {
+        return Integer.parseInt(token);
+    }
+
+    private static String scanString(String token) {
+        return token;
+    }
+
+    private static void print(Object obj) {
+        System.out.print(obj);
+    }
+
+    private static void println(Object obj) {
+        System.out.println(obj);
+    }
+}
